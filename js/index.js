b81097b8ec8eb0a839ffcbf8eb94c14e869bb4b7
webmaaster <wecodewetest@gmail.com>
2020-01-25 19:24:54 -0500

    Transferred all files and folders into web-intro

---
+++ js/index.js
@@ -0,0 +1,6 @@
+alert("Welcome");
+alert("You are really genius");
+alert("Learn web development and make $100k per year");
+alert("Nothing is impossible as impossible itself says I/M/Possible");
+alert("Learn today, earn tomorrow from anywhere in the world");
+alert("Contact us: softwarequalityexperts@gmail.com");
